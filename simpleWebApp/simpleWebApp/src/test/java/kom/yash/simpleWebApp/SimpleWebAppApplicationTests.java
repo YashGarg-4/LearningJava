be386f437969d764447c9e46f30dd253165c5eb0
YashGarg-4 <yashgarg04@gmail.com>
2025-06-03 17:02:04 +0530

    Starting with the webApp

---
+++ simpleWebApp/simpleWebApp/src/test/java/kom/yash/simpleWebApp/SimpleWebAppApplicationTests.java
@@ -0,0 +1,13 @@
+package kom.yash.simpleWebApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimpleWebAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
