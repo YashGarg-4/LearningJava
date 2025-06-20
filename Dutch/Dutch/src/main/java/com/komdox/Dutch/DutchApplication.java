203985372c26559079059f3358f07b3b6caf8fa2
YashGarg-4 <yashgarg04@gmail.com>
2025-06-20 18:58:52 +0530

    Starting work on Dutch w SprinAI

---
+++ Dutch/Dutch/src/main/java/com/komdox/Dutch/DutchApplication.java
@@ -0,0 +1,13 @@
+package com.komdox.Dutch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DutchApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DutchApplication.class, args);
+	}
+
+}
